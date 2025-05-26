5e85655876f80146b42319f954a6aa18931d307c
nitkach <nikita.tkachev2001@gmail.com>
2025-05-26 04:49:47 +0300

    Add rust toolchain

--- devcontainer-decondenser/install.sh
+++ devcontainer-decondenser/install.sh
@@ -8,6 +8,14 @@ node_version="20.19.2"
 
 mkdir -p "$HOME/.node"
 
-# Download and install nvm:
-fetch https://nodejs.org/dist/v$node_version/node-v$node_version-linux-x64.tar.xz | \
-    step tar -xJf - -C "$HOME/.node" --strip-components=1
+# Download and install node:
+fetch https://nodejs.org/dist/v$node_version/node-v$node_version-linux-x64.tar.xz \
+    | step tar -xJf - -C "$HOME/.node" --strip-components=1
+
+
+rust_version="1.87.0"
+
+# Download and run `rustup-init.sh` (-y argument disables confirmation prompts)
+# to install rust toolchain
+fetch --proto '=https' --tlsv1.2 https://sh.rustup.rs \
+    | sh -s -- -y --default-toolchain $rust_version --no-modify-path
