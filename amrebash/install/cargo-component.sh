6d3e8f03dadd9a86916e2814359e0c3d6411795c
MareStare <mare.stare.official@gmail.com>
2025-06-04 03:12:12 +0000

    Remove `cargo-component`. Add wasm-wasip2

--- amrebash/install/cargo-component.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-. "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
-
-# The are no precompiled binaries for the latest release at the time of this
-# writing. See: https://github.com/bytecodealliance/cargo-component/issues/397
-cargo install cargo-component --locked --version "$version" --color always
--- devcontainer-decondenser/setup.sh
+++ devcontainer-decondenser/setup.sh
@@ -5,19 +5,6 @@ set -euo pipefail
 . "$(dirname "${BASH_SOURCE[0]}")/../amrebash/utils/lib.sh"
 
 step ./amrebash/install/node.sh 20.19.2
-step ./amrebash/install/rust.sh 1.87.0 --target wasm32-unknown-unknown
+step ./amrebash/install/rust.sh 1.87.0 --target wasm32-wasip2
 step ./amrebash/install/taplo.sh 0.10.0
 step ./amrebash/install/typos.sh 1.32.0
-
-step sudo apt-get update -y
-step sudo apt-get install -y --no-install-recommends --no-install-suggests \
-    build-essential \
-    pkg-config \
-    libssl-dev
-
-# Builds from source, requires Rust toolchain
-step ./amrebash/install/cargo-component.sh 0.21.1
-
-step sudo rm -rf /var/lib/apt/lists/*
-step rm -rf "$HOME/.cargo/registry"
-step rm -rf "$HOME/.cargo/git"
