0ae85f7717de11ad680fa51e31ad1a050d4acb94
MareStare <mare.stare.official@gmail.com>
2025-06-07 17:32:38 +0000

    Add `wasm32-unknown-unknown`

--- devcontainer-decondenser/init.sh
+++ devcontainer-decondenser/init.sh
@@ -24,7 +24,9 @@ step ./scripts/install/typos.sh 1.32.0
 step ./scripts/install/cargo-component.sh dev
 
 step ./scripts/install/node.sh 20.19.2
-step ./scripts/install/rust.sh 1.87.0 --target wasm32-wasip1
+step ./scripts/install/rust.sh 1.87.0 \
+    --target wasm32-wasip1 \
+    --target wasm32-unknown-unknown
 
 step rm -rf "$HOME/.cargo/registry"
 step rm -rf "$HOME/.cargo/git"
